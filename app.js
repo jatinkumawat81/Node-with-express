ad26e0d9bd6bed8a8aff62e6331a40d886ee264b
jatin <jatinkumawat530@gmail.com>
2025-05-19 23:57:34 +0530

    changes

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ app.all('*',(req,res,next)=>{
     // const err = new Error(`Can't find ${req.originalUrl} on the server!`);
     // err.status = 'fail';
     // err.statusCode = 404;
-
     const err = new CustomError(`Can't find ${req.originalUrl} on the server!`,404);
     next(err)
 })
